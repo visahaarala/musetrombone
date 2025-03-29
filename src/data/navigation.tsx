97e824eb185e4d0feb1af8905b2aa5a0083ad7c7
Visa Haarala <visa.haarala@gmail.com>
2025-03-29 17:40:05 +0200

    instructions for navigation

--- src/data/navigation.tsx
+++ src/data/navigation.tsx
@@ -6,26 +6,36 @@ import Music from '../components/Music';
 import multiLanguage from '../utils/multiLanguage';
 
 export const navLinks = (lang: Language): NavLink[] => [
+  //
+  //
+  // Only modify the "name" (for jp & en) if necessary
+  //
+  // do not change "link" or "element"
+
   {
     name: multiLanguage('muse'),
-    link: 'muse',
-    element: <Muse lang={lang} />,
+    link: 'muse', // don't change
+    element: <Muse lang={lang} />, // don't change
   },
   {
     name: multiLanguage('concerts'),
-    link: 'concerts',
-    element: <Concerts lang={lang} />,
+    link: 'concerts', // don't change
+    element: <Concerts lang={lang} />, // don't change
   },
   {
     name: multiLanguage('music'),
-    link: 'music',
-    element: <Music lang={lang} />,
+    link: 'music', // don't change
+    element: <Music lang={lang} />, // don't change
   },
-  { name: multiLanguage('contact'), link: 'contact', element: <Contact /> },
+  {
+    name: multiLanguage('contact'),
+    link: 'contact', // don't change
+    element: <Contact />, // don't change
+  },
 ];
 
 export const footer: NavLink = {
   name: multiLanguage('links'),
-  link: 'links',
-  element: <Footer />,
+  link: 'links', // don't change
+  element: <Footer />, // don't change
 };
